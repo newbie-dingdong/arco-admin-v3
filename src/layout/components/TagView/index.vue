51fec1d0fb5b094d268629a8f98b5ff07cce6fd6
范才桂 <2470530249@qq.com>
2023-12-05 13:25:49 +0800

    fix: 标签栏优化

--- src/layout/components/TagView/index.vue
+++ src/layout/components/TagView/index.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import useTags from '@/store/app/tags';
 
-import useTags from '@/store/app/tags'
-
-const { name } = useRoute()
-const useTag = useTags()
+const useTag = useTags();
 </script>
 
 <template>
-  <a-scrollbar style="overflow: auto;">
-    <div class="flex border-t-1 gap-10px border-t-solid border-t-#eee bg-#fff py-1 px-3 items-center flex-nowrap w2000px">
-      <a-tag
-v-for="(item, index) in useTag.tagList" :key="item.name" checkable :checked="item.name === useTag.activeName"
-        :color="item.name === useTag.activeName ? '#165dff' : '#cfcccc'" closable size="medium"
-        @check="useTag.checkTag(item)" @close="useTag.removeTag(index)">{{ item.title }}</a-tag>
-    </div>
-  </a-scrollbar>
+  <div class="bg-#fff border-t-solid  border-t-1  border-t-#eee px-3">
+    <a-scrollbar style="overflow: auto;">
+      <div class="flex gap-10px py-1 items-center flex-nowrap">
+        <div v-for="(item, index) in useTag.tagList" :key="item.name" class="bg-#fff">
+          <a-tag
+checkable :checked="item.name === useTag.activeName"
+            :color="item.name === useTag.activeName ? '#165dff' : '#cfcccc'" closable size="medium"
+            @check="useTag.checkTag(item)" @close="useTag.removeTag(index)">{{ item.title }}</a-tag>
+        </div>
+      </div>
+    </a-scrollbar>
+  </div>
 </template>
